1d2b87fdbf34b04de38eb6963844303b2640b322
nihalerdal <nihalerdall@gmail.com>
2025-06-05 14:22:51 -0700

    Add session-based auth views, routes, and controller setup

--- models/User.js
+++ models/User.js
@@ -31,15 +31,15 @@ UserSchema.pre("save", async function (next) {
   next();
 });
 
-UserSchema.methods.createJWT = function () {
-  return jwt.sign(
-    { userId: this._id, name: this.name },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: process.env.JWT_LIFETIME,
-    }
-  );
-};
+// UserSchema.methods.createJWT = function () {
+//   return jwt.sign(
+//     { userId: this._id, name: this.name },
+//     process.env.JWT_SECRET,
+//     {
+//       expiresIn: process.env.JWT_LIFETIME,
+//     }
+//   );
+// };
 
 UserSchema.methods.comparePassword = async function (candidatePassword) {
   const isMatch = await bcrypt.compare(candidatePassword, this.password);
