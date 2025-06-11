3e81213d584f93cdfecd9978bd771bac995a4d3e
nihalerdal <nihalerdall@gmail.com>
2025-06-10 18:33:51 -0700

    Fix user id

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -1,9 +1,8 @@
 const Expense = require("../models/Expense");
-// const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllExpenses = async (req, res, next) => {
   try {
-    const expenses = await Expense.find({ createdBy: req.user.userId }).sort(
+    const expenses = await Expense.find({ createdBy: req.user._id }).sort(
       "createdAt"
     );
     res.render("expenses", { expenses, csrfToken: req.csrfToken()});
@@ -18,7 +17,7 @@ const getNewExpenseForm = (req, res) => {
 
 const createExpense = async (req, res, next) => {
   try {
-    req.body.createdBy = req.user.userId;
+    req.body.createdBy = req.user._id;
     await Expense.create(req.body);
     res.redirect("/expenses"); // Show updated list
   } catch (error) {
@@ -30,11 +29,13 @@ const getEditExpenseForm = async (req, res, next) => {
   try {
     const expense = await Expense.findOne({
       _id: req.params.id,
-      createdBy: req.user.userId,
+      createdBy: req.user._id,
     });
-    if (!expense) {
-      throw new NotFoundError("Expense not found");
-    }
+   if (!expense) {
+     const error = new Error("Expense not found");
+     error.statusCode = 404;
+     throw error;
+   }
     res.render("edit-expense", { expense, csrfToken: req.csrfToken() });
   } catch (error) {
     next(error);
@@ -46,7 +47,7 @@ const updateExpense = async (req, res, next) => {
   try {
     const { id } = req.params;
     const updated = await Expense.findOneAndUpdate(
-      { _id: id, createdBy: req.user.userId },
+      { _id: id, createdBy: req.user._id },
       req.body,
       { new: true, runValidators: true }
     );
@@ -64,7 +65,7 @@ const deleteExpense = async (req, res, next) => {
   try {
     await Expense.findOneAndDelete({
       _id: req.params.id,
-      createdBy: req.user.userId,
+      createdBy: req.user._id,
     });
     res.redirect("/expenses");
   } catch (error) {
